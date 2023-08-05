fbd2cb6257e29dc91b1d6028bc761cb1c59bca56
Mahendhiran <mahendhiran2506@gmail.com>
2023-08-04 22:07:51 -0700

    design enhanced

--- src/App.js
+++ src/App.js
@@ -1,31 +1,68 @@
 import "./styles.css";
-
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 import words from "./data";
 
 export default function App() {
-  const [word, setWord] = useState(["No word", "No meaning"]);
+  const [word, setWord] = useState("No word");
+  const [tamilWord, setTamilWord] = useState("");
+  const [phonetic, setPhonetic] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+    axios
+      .get(url)
+      .then((res) => {
+        const phoneticWord = res.data[0].phonetic;
+        setPhonetic(phoneticWord);
+      })
+      .catch((error) => {
+        setPhonetic("");
+        console.log(error);
+      });
+  }, [word]);
+
+  useEffect(() => {
+    const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ta&dt=t&q=${encodeURI(
+      word
+    )}`;
+    axios
+      .get(url)
+      .then((res) => {
+        setTamilWord(res.data[0][0][0]);
+      })
+      .catch((error) => {
+        setTamilWord("");
+        console.log(error);
+      });
+  }, [word]);
+
   const getRandomNumber = () => {
     return Math.floor(Math.random() * words.length);
   };
+
   const randonWordsHandler = () => {
     setWord(words[getRandomNumber()]);
   };
+
   const wordsMeaningHandler = () => {
     setIsOpen(!isOpen);
   };
+
   return (
     <div className="app-main">
       <div className="app-container">
         <p className="app-title">Random words</p>
-        <div className="app-word" onClick={wordsMeaningHandler}>
-          <div className="word-left">{word[0]}</div>
+        <div className="word-con word" onClick={wordsMeaningHandler}>
+          <div className="word-left">
+            {word[0].toUpperCase() + word.substring(1)}
+          </div>
         </div>
+        <p className="word-phonetic">{phonetic}</p>
         {isOpen && (
-          <div className="app-word" onClick={wordsMeaningHandler}>
-            <div className="word-left">{word[1]}</div>
+          <div className="word-con word-meaning" onClick={wordsMeaningHandler}>
+            <div className="word-left">{tamilWord}</div>
           </div>
         )}
         <div className="app-btn-grb">
